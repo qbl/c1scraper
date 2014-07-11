4aa79fa8950a299375e0e4874076b6d619002b63
Iqbal Farabi <farabi.iqbal@gmail.com>
2014-07-11 09:44:51 +0700

    First commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+C1scraper::Application.config.secret_token = '55f9f291acbf440d826bc76b51b689f9c10df8d26c11c59423e8ccf0cb36d0d6345219b3ed1d8bfbddf9a1e3ed9d11cda2af8268e4e6dbd38f31014beb6b0383'
